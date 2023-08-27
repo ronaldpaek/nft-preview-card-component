bf02447d7b833f5c313c45a4a17e5298f2303ab8
Ronald Paek <ronaldpaek@gmail.com>
2023-08-26 19:27:52 -0700

    Init project

---
+++ postcss.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: {
+    autoprefixer: {},
+    "postcss-custom-media": {},
+    "postcss-import": {},
+    "postcss-media-minmax": {},
+    "postcss-mixins": {},
+    "postcss-preset-env": {},
+    "postcss-nesting": {},
+  },
+};
